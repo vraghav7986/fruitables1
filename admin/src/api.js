fcd93c1cb418f8ce85c86205c65e22a1f35710bc
Raghav Verma <vraghav7986@gmail.com>
2026-04-02 12:50:57 +0530

    Update frontend: fix API URL and ESLint warnings

--- admin/src/api.js
+++ admin/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:9999';
+const API_URL = "https://fruitables-backend-n8nj.onrender.com";
 
 const api = axios.create({
   baseURL: API_URL,
